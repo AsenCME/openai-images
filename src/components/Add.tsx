bb99edc01dc99fa69048afe059dbe5c6e7d45141
Asen Georgiev <asen.connor@abv.bg>
2022-11-07 18:25:37 +0200

    fail

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,23 +1,71 @@
 import { createSignal } from "solid-js";
+import { addEntryToday, AddTodayData, Client } from "../db";
 
-export default function Add() {
+export default function Add({ dbClient }: { dbClient: Client }) {
+  const [form, setForm] = createSignal<AddTodayData>({
+    description: "",
+    amount: 0,
+    currency: "BGN",
+    source: "cash",
+    account: "personal",
+  });
   const [more, setMore] = createSignal(false);
+  const [loading, setLoading] = createSignal(false);
+  const add = async () => {
+    setLoading(true);
+    await addEntryToday(dbClient, form());
+    setLoading(false);
+  };
+
   return (
-    <div class="flex flex-col md:flex-row w-full gap-4 justify-between items-center">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-1">
+    <div class="flex flex-col w-full gap-4 max-w-xl mx-auto">
+      <div
+        class="px-4 py-2 rounded bg-lime-800 hoverable text-center"
+        onClick={add}
+      >
+        <span class="font-bold text-white">add</span>
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-1 last:col-span-2">
         <div>
           <div>Description</div>
-          <input type="text" />
+          <input
+            type="text"
+            value={form().description}
+            onChange={(e) =>
+              setForm((prev) => ({
+                ...prev,
+                description: e.currentTarget.value,
+              }))
+            }
+          />
         </div>
         <div>
           <div>Amount</div>
-          <input type="text" />
+          <input
+            type="number"
+            value={form().amount}
+            onChange={(e) =>
+              setForm((prev) => ({
+                ...prev,
+                amount: Number(e.currentTarget.value),
+              }))
+            }
+          />
         </div>
         {more() ? (
           <>
             <div>
               <div>Currency</div>
-              <select>
+              <select
+                value={form().currency}
+                onChange={(e) =>
+                  setForm((prev) => ({
+                    ...prev,
+                    currency: e.currentTarget.value as any,
+                  }))
+                }
+              >
                 <option value="BGN">BGN</option>
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
@@ -25,21 +73,41 @@ export default function Add() {
             </div>
             <div>
               <div>Source</div>
-              <select>
+              <select
+                value={form().source}
+                onChange={(e) =>
+                  setForm((prev) => ({
+                    ...prev,
+                    source: e.currentTarget.value as any,
+                  }))
+                }
+              >
                 <option value="cash">Cash</option>
                 <option value="card">Card</option>
               </select>
             </div>
             <div>
               <div>Account</div>
-              <select>
+              <select
+                value={form().account}
+                onChange={(e) =>
+                  setForm((prev) => ({
+                    ...prev,
+                    account: e.currentTarget.value as any,
+                  }))
+                }
+              >
                 <option value="personal">Personal</option>
                 <option value="business">Business</option>
               </select>
             </div>
           </>
         ) : null}
+      </div>
 
+      {loading() ? (
+        <div>Loading...</div>
+      ) : (
         <div
           class="col-span-2 flex gap-2 items-center cursor-pointer hoverable bg-gray-200 justify-center mt-2 p-2 rounded"
           onClick={() => setMore(!more())}
@@ -48,10 +116,7 @@ export default function Add() {
             Show {more() ? "less" : "more"}
           </div>
         </div>
-      </div>
-      <div class="px-4 py-2 rounded bg-lime-800 hoverable">
-        <span class="font-bold text-white">add</span>
-      </div>
+      )}
     </div>
   );
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,32 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
+export type Client = SupabaseClient<any, "public", any>;
 
 export const getClient = async (key) => {
   return createClient("https://ffuhrsttsgpivjbywouf.supabase.co", key);
 };
 
-export const getToday = async (client: SupabaseClient<any, "public", any>) => {
+export const getToday = async (client: Client) => {
   const { data, error } = await client.from("today").select("*");
   if (error) {
     console.log("There was an error", error);
     return null;
   }
   return data;
+};
+
+export type AddTodayData = {
+  description: string;
+  amount: number;
+  currency: "BGN" | "USD" | "EUR";
+  source: "cash" | "card";
+  account: "personal" | "business";
+};
+export const addEntryToday = async (client: Client, data: AddTodayData) => {
+  try {
+    const { error } = await client.from("expense").insert(data);
+    if (error) return error.message;
+    return null;
+  } catch (error) {
+    return JSON.stringify(error);
+  }
 };
